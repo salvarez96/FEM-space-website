3a2266c2f1813c59d61717a4e3ba62a7dd0c64c8
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-05-01 22:34:31 -0500

    Create navigation menu functionality in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './global-styles.scss'
 import Header from './header/header'
+import Home from './pages/home'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
--- src/header/header.tsx
+++ src/header/header.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import styles from './header.module.scss'
 import { navigationItems } from './navigation-items'
 
-// const navigationItems = ["00 HOME", "01 DESTINATION", "02 CREW", "03 DESTINATION"]
-
 export default function Header() {
   return (
     <header min-heigth={96}>
+      <input type="checkbox" id="hamburger-menu-toggle" />
       <div className={styles['header-icons-container']}>
         <img className={styles['logo']} src="/shared/logo.svg" alt="App logo" width={40} height={40} />
         <label htmlFor="hamburger-menu-toggle">
           <img className={styles['hamburger-icon']} src="/shared/icon-hamburger.svg" alt="click to open navigation" width={24} height={21} />
         </label>
       </div>
-      <input type="checkbox" id="hamburger-menu-toggle" />
       <div className={styles['navigation-container']}>
-        <img className={styles['close-navigation']} src="/shared/icon-close.svg" alt="click to close navigation" width={19} height={19} />
+        <label htmlFor="hamburger-menu-toggle" className={styles['close-navigation-label']}>
+          <img className={styles['close-navigation']} src="/shared/icon-close.svg" alt="click to close navigation" width={19} height={19} />
+        </label>
         <nav>
           <ul>
             {navigationItems.map((navItem, i) => (
