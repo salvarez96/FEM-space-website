11561182157aba7cd8e956ff5b5d4882af9e0c98
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-04 14:16:52 -0500

    💄 Update and improve project header
    Add tablet and desktop header views. Improve header mobile user experience.

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -4,24 +4,28 @@ import { navigationItems } from './navigation-items'
 
 export default function Header() {
   return (
-    <header min-heigth={96}>
+    <header min-height={96}>
       <input type="checkbox" id="hamburger-menu-toggle" />
+
       <div className={styles['header-icons-container']}>
         <img className={styles['logo']} src="/shared/logo.svg" alt="App logo" width={40} height={40} />
         <label htmlFor="hamburger-menu-toggle">
           <img className={styles['hamburger-icon']} src="/shared/icon-hamburger.svg" alt="click to open navigation" width={24} height={21} />
         </label>
       </div>
+
       <div className={styles['navigation-container']}>
         <label htmlFor="hamburger-menu-toggle" className={styles['close-navigation-label']}>
           <img className={styles['close-navigation']} src="/shared/icon-close.svg" alt="click to close navigation" width={19} height={19} />
         </label>
         <nav>
+
           <ul>
             {navigationItems.map((navItem, i) => (
               <li key={i}><span className={styles['navigation-item-number']}>{navItem.groupNumber}</span> {navItem.navTitle}</li>
             ))}
           </ul>
+
         </nav>
       </div>
     </header>
