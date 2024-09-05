bb62d987ac052dd05e5b1cbf066e1671b1e2da0e
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-04 20:37:39 -0500

    💄 Create project's home page
    Create project's home page for all devices (mobile, tablet, desktop).

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { homeCopies } from '@copies'
+import styles from '@styles/home.module.scss'
 
 export default function Home() {
   return (
-    <div></div>
+    <div className={ styles['parent-container'] }>
+      <div className={ styles['home-container'] }>
+        <section className={ styles['titles-container'] }>
+          <h6>{ homeCopies['subtitle'] }</h6>
+          <h1>{ homeCopies['title'] }</h1>
+        </section>
+        <section className={`${styles['main-content-container']}  main-text-container` }>
+          <p>{ homeCopies['main-content'] }</p>
+        </section>
+      </div>
+      <button className={ styles['home-button'] } type="button">Explore</button>
+    </div>
   )
 }
