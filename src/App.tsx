f1193cf7bfb2467a75aff891e14faf156b1aa7fd
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-05 19:07:05 -0500

    ✨ Create routes with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import '@styles/global-styles.scss'
 import Header from '@components/header/header'
+import { Outlet, useLocation } from 'react-router-dom'
 import Home from '@pages/home'
 
 function App() {
 
+  const getLocation = useLocation().pathname
+
   return (
     <>
       <Header />
-      <Home />
+      { getLocation === '/' ? <Home /> : <Outlet />}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Home from '@pages/home'
+import Destination from '@pages/destination'
+import Crew from '@pages/crew'
+import Technology from '@pages/technology'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='home' element={<Home />} />
+      <Route path='destination' element={<Destination />} />
+      <Route path='crew' element={<Crew />} />
+      <Route path='technology' element={<Technology />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
