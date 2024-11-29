e3c4d418012d8b278fd20364209fb13cc98817f4
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-11-28 22:26:35 -0500

    ✨ Add interactivity to destination page
    Add interactive custom URLs to change data. Use URL params to dynamically change data. Create additional containers for desktop styles.

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -1,33 +1,58 @@
 import { destinationCopies } from '@copies'
 import styles from '@styles/pages/destination.module.scss'
+import { useEffect, useState } from 'react'
+import { NavLink, useLocation, useParams } from 'react-router-dom'
 
 export default function Destination() {
+
+  let { destinationName } = useParams()
+  const currentLocation = useLocation().pathname
+  const [ destinationToRender, setDestinationToRender ] = useState(destinationCopies[0])
+
+  useEffect(() => {
+    destinationCopies.find(destination => {
+      if (destination.heading.toLowerCase() === destinationName) {
+        setDestinationToRender(destination)
+        return true
+      }
+    })
+  }, [destinationName])
+
+  function checkCurrentLocation(destination: typeof destinationToRender, index: number) {
+    return currentLocation.includes(destination.heading.toLowerCase()) || (currentLocation === '/destination' && !index)
+  }
+
   return (
-    <>
-      <h5 className={styles['destination-subheading']}><span>01</span>PICK YOUR DESTINATION</h5>
-      <img className={ styles['destination-image'] } src={destinationCopies[0].image} alt={destinationCopies[0].heading + ' image'} />
-      <nav className={styles['destination-navigation']}>
-        <ul>
-          {destinationCopies.map(destination => (
-                <li key={destination.heading}>{destination.heading}</li>
-          ))}
-        </ul>
-      </nav>
+    <div className={styles['destination-page-container']}>
+      <section className={styles['destination-complimentary-container']}>
+        <h5 className={styles['destination-subheading']}><span>01</span>PICK YOUR DESTINATION</h5>
+        <img className={ styles['destination-image'] } src={destinationToRender.image} alt={destinationToRender.heading + ' image'} />
+      </section>
       <section className={styles['destination-main-container']}>
-        <h2 className={styles['destination-heading']}>{destinationCopies[0].heading}</h2>
-        <p className={styles['main-content']}>{destinationCopies[0]['main-content']}</p>
+        <nav className={styles['destination-navigation']}>
+          <ul>
+            {destinationCopies.map((destination, index) => (
+              <li key={destination.heading}>
+                <NavLink className={ checkCurrentLocation(destination, index) ? styles['navigation-link__active'] : '' } to={'/destination/' + destination.heading.toLowerCase()}>{destination.heading}</NavLink>
+                <div className={ styles[`navigation-item__${checkCurrentLocation(destination, index) ? 'active' : 'hover'}`] }></div>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <h2 className={styles['destination-heading']}>{destinationToRender.heading}</h2>
+        <p className={styles['main-content']}>{destinationToRender['main-content']}</p>
         <div className={styles['decorator']}></div>
         <article className={styles['additional-facts-container']}>
           <div className={styles["additional-facts"]}>
             <p className={styles["additional-facts-title"]}>avg. distance</p>
-            <p className={styles["additional-facts-fact"]}>{destinationCopies[0].additionalFacts.averageDistance}</p>
+            <p className={styles["additional-facts-fact"]}>{destinationToRender.additionalFacts.averageDistance}</p>
           </div>
           <div className={styles["additional-facts"]}>
             <p className={styles["additional-facts-title"]}>est. travel time</p>
-            <p className={styles["additional-facts-fact"]}>{destinationCopies[0].additionalFacts.travelTime}</p>
+            <p className={styles["additional-facts-fact"]}>{destinationToRender.additionalFacts.travelTime}</p>
           </div>
         </article>
       </section>
-    </>
+    </div>
   )
 }
