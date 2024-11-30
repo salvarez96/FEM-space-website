7ff03b1938c29bbc7aadfb66d2557f7a42f466fa
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-11-29 19:06:05 -0500

    💄 Add crew page tablet and desktop styles

--- src/pages/crew.tsx
+++ src/pages/crew.tsx
@@ -9,6 +9,7 @@ export default function Crew() {
   let { crewMember } = useParams()
   const currentLocation = useLocation().pathname
   const [ crewMemberToRender, setCrewMemberToRender ] = useState(crewCopies[0])
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   useEffect(() => {
     crewCopies.find(member => {
@@ -19,31 +20,67 @@ export default function Crew() {
     })
   }, [crewMember])
 
+  // handle content rendering depending on window dimensions
+  let isMobileWidth = windowWidth < 768 ? true : false
+
+  window.addEventListener('resize', () => {
+    setWindowWidth(window.innerWidth)
+  });
+
   return (
     <div className={styles['crew-page-container']}>
-      <section className={styles['crew-complimentary-container']}>
-        <h5 className={styles['crew-subheading']}><span>02</span>MEET YOUR CREW</h5>
+      {isMobileWidth ?
+      // mobile content
+      <>
+        <section className={styles['crew-complimentary-container']}>
+          <h5 className={styles['crew-subheading']}><span>02</span>MEET YOUR CREW</h5>
+          <img className={ styles['crew-image'] } src={crewMemberToRender.image} alt={crewMemberToRender.name + ' image'} />
+          <div className={styles['decorator']}></div>
+        </section>
+        <section className={styles['crew-main-container']}>
+          <nav className={styles['crew-navigation']}>
+            <ul>
+              {crewCopies.map(({ name }, index) => (
+                <li key={ name }>
+                  <NavLink
+                    className={ styles[`navigation-item${checkCurrentUrl(name, index, currentLocation, '/crew') ? '__active' : ''}`] }
+                    to={'/crew/' + transformToUrlString(name)}
+                    title={ name }
+                  ></NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <h4 className={styles['crew-position']}>{crewMemberToRender.position}</h4>
+          <h2 className={styles['crew-heading']}>{crewMemberToRender.name}</h2>
+          <p className={styles['main-content']}>{crewMemberToRender['main-content']}</p>
+        </section>
+      </>
+      : // desktop content
+      <>
+        <section className={styles['crew-complimentary-container']}>
+          <h5 className={styles['crew-subheading']}><span>02</span>MEET YOUR CREW</h5>
+        </section>
+        <section className={styles['crew-main-container']}>
+          <h4 className={styles['crew-position']}>{crewMemberToRender.position}</h4>
+          <h2 className={styles['crew-heading']}>{crewMemberToRender.name}</h2>
+          <p className={styles['main-content']}>{crewMemberToRender['main-content']}</p>
+          <nav className={styles['crew-navigation']}>
+            <ul>
+              {crewCopies.map(({ name }, index) => (
+                <li key={ name }>
+                  <NavLink
+                    className={ styles[`navigation-item${checkCurrentUrl(name, index, currentLocation, '/crew') ? '__active' : ''}`] }
+                    to={'/crew/' + transformToUrlString(name)}
+                    title={ name }
+                  ></NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </section>
         <img className={ styles['crew-image'] } src={crewMemberToRender.image} alt={crewMemberToRender.name + ' image'} />
-        <div className={styles['decorator']}></div>
-      </section>
-      <section className={styles['crew-main-container']}>
-        <nav className={styles['crew-navigation']}>
-          <ul>
-            {crewCopies.map(({ name }, index) => (
-              <li key={ name }>
-                <NavLink
-                  className={ styles[`navigation-item${checkCurrentUrl(name, index, currentLocation, '/crew') ? '__active' : ''}`] }
-                  to={'/crew/' + transformToUrlString(name)}
-                  title={ name }
-                ></NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <h4 className={styles['crew-position']}>{crewMemberToRender.position}</h4>
-        <h2 className={styles['crew-heading']}>{crewMemberToRender.name}</h2>
-        <p className={styles['main-content']}>{crewMemberToRender['main-content']}</p>
-      </section>
-    </div>
-  )
+      </> 
+    }
+  </div>)
 }
